3d7bf75e95f0ba4860dc9ea1ac918ecc0006989d
peter <peter@peters-mbp-3.broadband>
2020-02-28 11:22:11 +0000

    Final tidy up.
    - Moving Configurable properties load into Configurables class
    - Creating Constants
    - Adding maven build plugins

--- mainmodule/runnermodule/src/main/java/com/sainsburys/integration/runnermodule/Main.java
+++ mainmodule/runnermodule/src/main/java/com/sainsburys/integration/runnermodule/Main.java
@@ -1,11 +1,5 @@
 package com.sainsburys.integration.runnermodule;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sainsburys.integration.parsingmodule.JSoupParser;
 import com.sainsburys.integration.runnermodule.model.Response;
@@ -14,25 +8,8 @@ import com.sainsburys.integration.scrapermodule.JSoupScraper;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		Runner runner = new Runner();
-		Response response = runner.init(new JSoupScraper(), new JSoupParser(), initConfig());
+		Response response = runner.init(new JSoupScraper(), new JSoupParser(), Configurable.init());
 		ObjectMapper Obj = new ObjectMapper();
 		System.out.println(Obj.writerWithDefaultPrettyPrinter().writeValueAsString(response));
-	}
-
-	private static Configurable initConfig() throws Exception {
-		Properties prop = new Properties();
-		String fileName = "src/main/resources/config.properties";
-		InputStream is = null;
-		try {
-		    is = new FileInputStream(fileName);
-		} catch (FileNotFoundException ex) {
-		    throw new Exception("Properties file not found");
-		}
-		try {
-		    prop.load(is);
-		} catch (IOException ex) {
-		    throw new Exception("Properties failed to load");
-		}
-		return new Configurable(prop.getProperty("scraper.url"), prop.getProperty("parser.urlTagName"), prop.getProperty("parser.domain"), prop.getProperty("parser.titleQuery"), prop.getProperty("parser.descriptionQuery"), prop.getProperty("parser.pricePerUnitQuery"), prop.getProperty("parser.kcal100gQuery"), Double.parseDouble(prop.getProperty("vat")));
 	}
 }
--- mainmodule/runnermodule/src/main/java/com/sainsburys/integration/runnermodule/Runner.java
+++ mainmodule/runnermodule/src/main/java/com/sainsburys/integration/runnermodule/Runner.java
@@ -2,13 +2,13 @@ package com.sainsburys.integration.runnermodule;
 
 import java.util.ArrayList;
 
-import com.sainsburys.integration.parsingmodule.ParserInterface;
+import com.sainsburys.integration.parsingmodule.JSoupParser;
 import com.sainsburys.integration.parsingmodule.model.Result;
 import com.sainsburys.integration.runnermodule.model.Response;
-import com.sainsburys.integration.scrapermodule.ScraperInterface;
+import com.sainsburys.integration.scrapermodule.JSoupScraper;
 
 public class Runner {
-	public Response init(ScraperInterface scraper, ParserInterface parser, Configurable config) throws Exception {
+	public Response init(JSoupScraper scraper, JSoupParser parser, Configurable config) throws Exception {
 		String html;
 
 		html = scraper.scrape(config.getUrl());
